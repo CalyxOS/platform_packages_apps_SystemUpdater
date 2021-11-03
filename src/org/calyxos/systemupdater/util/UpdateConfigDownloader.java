db34acc46158ce0afccb43ff74d58c69f0027d27
Luca Stefani <luca.stefani.ge1@gmail.com>
2021-11-03 17:51:30 +0100

    Initial support for server provided OTA json
    Currently the "server" path is hardcoded to
    server/Build.DEVICE
    This need to be changed to support different channels
    
    Change-Id: I6f4d821c3150f1fdcf1786a40565b1f82ce3fe47

---
+++ src/org/calyxos/systemupdater/util/UpdateConfigDownloader.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *               2021 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.calyxos.systemupdater.util;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.Log;
+
+import org.calyxos.systemupdater.R;
+import org.calyxos.systemupdater.UpdateConfig;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Optional;
+
+/**
+ * Utility class for working with json update configurations.
+ */
+public final class UpdateConfigDownloader {
+    private UpdateConfigDownloader() {
+    }
+
+    /**
+     * @return update config from remote server
+     */
+    public static UpdateConfig getUpdateConfig(Context context) {
+        String server = context.getResources().getString(R.string.server);
+        try {
+            JsonDownloader d = new JsonDownloader(url + Build.DEVICE);
+            String json = d.download();
+            return UpdateConfig.fromJson(json);
+        } catch (IOException | JSONException e) {
+        }
+        return null;
+    }
+
+    /**
+     * @param filename searches by given filename
+     * @param config   searches in {@link UpdateConfig#getAbConfig()}
+     * @return offset and size of {@code filename} in the package zip file
+     * stored as {@link UpdateConfig.PackageFile}.
+     */
+    public static Optional<UpdateConfig.PackageFile> getPropertyFile(
+            final String filename,
+            UpdateConfig config) {
+        return Arrays
+                .stream(config.getAbConfig().getPropertyFiles())
+                .filter(file -> filename.equals(file.getFilename()))
+                .findFirst();
+    }
+}
