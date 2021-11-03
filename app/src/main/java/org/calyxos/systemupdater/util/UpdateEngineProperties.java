d8c9970c09a0b72f5be394e969fb53cfdad4854a
Luca Stefani <luca.stefani.ge1@gmail.com>
2021-11-03 16:00:43 +0100

    Remove "slot switch" button
    We'll always switch slot and run post-install
    
    Change-Id: Iaaa7a7de11680e7131d238aec8c47810ece0a459

--- app/src/main/java/org/calyxos/systemupdater/util/UpdateEngineProperties.java
+++ app/src/main/java/org/calyxos/systemupdater/util/UpdateEngineProperties.java
@@ -20,18 +20,5 @@ package org.calyxos.systemupdater.util;
  * Utility class for properties that will be passed to {@code UpdateEngine#applyPayload}.
  */
 public final class UpdateEngineProperties {
-
-    /**
-     * The property indicating that the update engine should not switch slot
-     * when the device reboots.
-     */
-    public static final String PROPERTY_DISABLE_SWITCH_SLOT_ON_REBOOT = "SWITCH_SLOT_ON_REBOOT=0";
-
-    /**
-     * The property to skip post-installation.
-     * https://source.android.com/devices/tech/ota/ab/#post-installation
-     */
-    public static final String PROPERTY_SKIP_POST_INSTALL = "RUN_POST_INSTALL=0";
-
     private UpdateEngineProperties() {}
 }
