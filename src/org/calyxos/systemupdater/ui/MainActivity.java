1915a4b84fa112032ca49d72af216e0569a0e30b
Luca Stefani <luca.stefani.ge1@gmail.com>
2021-12-09 22:18:20 +0100

    Use an AsyncTask to download the JSON config
    We should be using Kotlin coroutines by now
    but introducing Kotlin just for this bit of code seems
    a waste. Let's just use the deprecated way
    
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Change-Id: I860c3ba961543e114494573727325be44ffd6047

--- src/org/calyxos/systemupdater/ui/MainActivity.java
+++ src/org/calyxos/systemupdater/ui/MainActivity.java
@@ -17,7 +17,9 @@
 package org.calyxos.systemupdater.ui;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -351,8 +353,26 @@ public class MainActivity extends AppCompatActivity {
      * loads json configurations from server in {@link R.string.server}.
      */
     private void loadUpdateConfig() {
-        // TODO Move to another thread
-        mConfig = UpdateConfigDownloader.getUpdateConfig();
+        new DownloadJsonTask(this).execute();
+    }
+
+    private class DownloadJsonTask extends AsyncTask<Void, Void, UpdateConfig> {
+        private Context mContext;
+
+        public DownloadJsonTask(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        protected UpdateConfig doInBackground(Void... params) {
+            return UpdateConfigDownloader.getUpdateConfig(mContext);
+        }
+
+        @Override
+        protected void onPostExecute(UpdateConfig updateConfig) {
+            mConfig = updateConfig;
+            uiResetWidgets();
+        }
     }
 
     /**
