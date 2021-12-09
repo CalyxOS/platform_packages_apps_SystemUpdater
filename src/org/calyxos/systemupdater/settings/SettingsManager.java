ce8cfd09a7241f7226cec9ee0a3b096d70b51e9c
Luca Stefani <luca.stefani.ge1@gmail.com>
2021-12-09 22:07:00 +0100

    Add initial SettingsManager
    This class will hold all the shared preferences that will be used
    in app to control its behaviour
    
    Each pref will be overridable via the prop sys.updater.$key
    
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Change-Id: I2e734d23682f2a29f131443c59067ccae93e70bb

---
+++ src/org/calyxos/systemupdater/settings/SettingsManager.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2021 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.calyxos.systemupdater.settings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+
+public class SettingsManager {
+    // Prop schema: ${PROP_BASE} + ${KEY_X}
+    private static final String PROP_BASE = "sys.updater.";
+
+    // Shared preferences keys, will be stored in protected storage
+    private static final String KEY_CHANNEL = "channel";
+
+    private static SharedPreferences getSharedPrefs(Context context) {
+        return context.createDeviceProtectedStorageContext().getSharedPreferences(
+                context.getPackageName(), Context.MODE_PRIVATE);
+    }
+
+    private static String getString(Context context, String key, String fallback) {
+        return getSharedPrefs(context).getString(key, fallback);
+    }
+
+    private static int getInt(Context context, String key, int fallback) {
+        return getSharedPrefs(context).getInt(key, fallback);
+    }
+
+    public static String getChannel(Context context) {
+        String fallback = context.getString(R.string.default_channel);
+        String pref = getString(context, KEY_CHANNEL, fallback);
+        return SystemProperties.get(PROP_BASE + KEY_CHANNEL, pref);
+    }
+}
