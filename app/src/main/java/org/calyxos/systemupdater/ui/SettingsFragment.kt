f5e93ea46eaece5c493826c592cea2106ae18c26
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-03-23 16:12:16 +0100

    Support fetching update config from remote server [2/3]
    This change introduces new UI for the Updater app that will fetch
    and showcase data from the remote server regarding updates.
    
    We are also switching to navigation components while at it with Kotlin
    to keep the app following Single Activity Arch as before.
    
    Change-Id: Ie990cdd820c5229518ccbeb948d0fbe019930ffe
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

---
+++ app/src/main/java/org/calyxos/systemupdater/ui/SettingsFragment.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2023 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.calyxos.systemupdater.ui
+
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.preference.ListPreference
+import androidx.preference.PreferenceFragmentCompat
+import com.google.android.material.appbar.MaterialToolbar
+import dagger.hilt.android.AndroidEntryPoint
+import org.calyxos.systemupdater.R
+import org.calyxos.systemupdater.util.CommonUtils.defaultChannel
+import org.calyxos.systemupdater.util.CommonUtils.updateChannel
+import javax.inject.Inject
+
+@AndroidEntryPoint(PreferenceFragmentCompat::class)
+class SettingsFragment : Hilt_SettingsFragment() {
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<MaterialToolbar>(R.id.toolbar).setNavigationOnClickListener {
+            view.findNavController().navigateUp()
+        }
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings_preferences, rootKey)
+
+        findPreference<ListPreference>("channel")?.apply {
+            setOnPreferenceChangeListener { _, newValue ->
+                summary = newValue.toString()
+                true
+            }
+            summary = sharedPreferences.getString(updateChannel, defaultChannel)
+        }
+    }
+}
