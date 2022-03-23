626390f80cc53e347da56227c5303403b4167989
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-03-23 16:12:16 +0100

    Support fetching update config from remote server [2/3]
    This change introduces new UI for the Updater app that will fetch
    and showcase data from the remote server regarding updates.
    
    We are also switching to navigation components while at it with Kotlin
    to keep the app following Single Activity Arch as before.
    
    Change-Id: Ie990cdd820c5229518ccbeb948d0fbe019930ffe
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

---
+++ app/src/main/java/org/calyxos/systemupdater/ui/UpdateFragment.kt
@@ -0,0 +1,248 @@
+/*
+ * Copyright (C) 2023 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.calyxos.systemupdater.ui
+
+import android.os.Build
+import android.os.Bundle
+import android.os.SystemProperties
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.progressindicator.LinearProgressIndicator
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import org.calyxos.systemupdater.R
+import org.calyxos.systemupdater.update.models.UpdateStatus
+
+@AndroidEntryPoint(Fragment::class)
+class UpdateFragment : Hilt_UpdateFragment(R.layout.fragment_update) {
+
+    private val TAG = UpdateFragment::class.java.simpleName
+    private val viewModel: UpdateViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<MaterialToolbar>(R.id.toolbar).apply {
+            setOnMenuItemClickListener {
+                if (it.itemId == R.id.settings) {
+                    view.findNavController().navigate(R.id.settingsFragment)
+                }
+                true
+            }
+        }
+
+        // Set CalyxOS, Android, Last Check and ASB values
+        view.apply {
+            findViewById<TextView>(R.id.androidVersion).text =
+                getString(R.string.android_version, Build.VERSION.RELEASE)
+            findViewById<TextView>(R.id.calyxVersion).text =
+                getString(R.string.calyx_version, SystemProperties.get("ro.calyxos.version"))
+            findViewById<TextView>(R.id.securityVersion).text =
+                getString(R.string.security_version, viewModel.getASBDate())
+
+            lifecycleScope.launchWhenStarted {
+                viewModel.updateLastCheck.collect {
+                    findViewById<TextView>(R.id.lastUpdateCheck).text =
+                        getString(R.string.last_check, it)
+                }
+            }
+        }
+
+        // Update views based on update' status
+        view.apply {
+            val updateTitle = findViewById<TextView>(R.id.updateTitle)
+            val updateCheck = findViewById<ProgressBar>(R.id.updateCheck)
+            val updateContainer = findViewById<LinearLayout>(R.id.updateContainer)
+            val updateButton = findViewById<Button>(R.id.updateButton)
+
+            val infoContainer = findViewById<LinearLayout>(R.id.infoContainer)
+            val installProgress = findViewById<LinearProgressIndicator>(R.id.updateInstallProgress)
+            val installSteps = findViewById<TextView>(R.id.updateInstallSteps)
+            val networkWarning = findViewById<TextView>(R.id.networkWarning)
+
+            lifecycleScope.launchWhenStarted {
+                viewModel.updateStatus.collect { status ->
+                    viewModel.saveLastUpdate()
+
+                    // Set update title based on status
+                    updateTitle.text = when (status) {
+                        UpdateStatus.IDLE -> {
+                            getString(R.string.uptodate)
+                        }
+                        UpdateStatus.CHECKING_FOR_UPDATE -> {
+                            getString(R.string.checking_updates)
+                        }
+                        UpdateStatus.UPDATED_NEED_REBOOT -> {
+                            getString(R.string.update_done)
+                        }
+                        UpdateStatus.UPDATE_AVAILABLE -> {
+                            getString(R.string.update_available)
+                        }
+                        UpdateStatus.FAILED_PREPARING_UPDATE,
+                        UpdateStatus.REPORTING_ERROR_EVENT -> {
+                            getString(R.string.updated_failed)
+                        }
+                        UpdateStatus.PREPARING_TO_UPDATE,
+                        UpdateStatus.DOWNLOADING,
+                        UpdateStatus.SUSPENDED,
+                        UpdateStatus.VERIFYING,
+                        UpdateStatus.FINALIZING -> {
+                            getString(R.string.installing_update)
+                        }
+                        else -> {
+                            String()
+                        }
+                    }
+
+                    // Set update stage as normal text in installSteps
+                    // Visibility is handled in the everything else block at the last
+                    installSteps.text = status.name.lowercase()
+                        .replace("_", " ")
+                        .replaceFirstChar { it.uppercase() }
+
+                    // Show or hide network warning
+                    networkWarning.visibility = when (status) {
+                        UpdateStatus.UPDATE_AVAILABLE,
+                        UpdateStatus.DOWNLOADING -> { View.VISIBLE }
+                        else -> { View.GONE }
+                    }
+
+                    // Set update button's behaviour based on status
+                    // Visibility is handled in the everything else block at the last
+                    updateButton.apply {
+                        when (status) {
+                            UpdateStatus.IDLE -> {
+                                text = getString(R.string.check_update)
+                                isEnabled = true
+                                setOnClickListener {
+                                    viewModel.checkUpdates()
+                                }
+                            }
+                            UpdateStatus.UPDATED_NEED_REBOOT -> {
+                                text = getString(R.string.reboot)
+                                isEnabled = true
+                                setOnClickListener {
+                                    // TODO: Reboot device
+                                }
+                            }
+                            UpdateStatus.UPDATE_AVAILABLE -> {
+                                text = getString(R.string.install)
+                                isEnabled = true
+                                setOnClickListener {
+                                    viewModel.applyUpdate()
+                                }
+                            }
+                            UpdateStatus.FAILED_PREPARING_UPDATE,
+                            UpdateStatus.REPORTING_ERROR_EVENT, -> {
+                                text = getString(R.string.retry)
+                                isEnabled = true
+                                setOnClickListener {
+                                    viewModel.checkUpdates()
+                                }
+                            }
+                            UpdateStatus.SUSPENDED -> {
+                                text = getString(R.string.resume)
+                                isEnabled = true
+                                setOnClickListener {
+                                    viewModel.resumeUpdate()
+                                }
+                            }
+                            UpdateStatus.PREPARING_TO_UPDATE,
+                            UpdateStatus.DOWNLOADING,
+                            UpdateStatus.VERIFYING,
+                            UpdateStatus.FINALIZING -> {
+                                isEnabled = status == UpdateStatus.DOWNLOADING
+                                text = getString(R.string.suspend)
+                                setOnClickListener {
+                                    viewModel.suspendUpdate()
+                                }
+                            }
+                            else -> {
+                                Log.d(TAG, "No update button action for: ${status.name}")
+                            }
+                        }
+                    }
+
+                    // Handle everything else based on status
+                    when (status) {
+                        UpdateStatus.IDLE -> {
+                            updateContainer.visibility = View.GONE
+                            infoContainer.visibility = View.VISIBLE
+                            updateCheck.visibility = View.GONE
+                            updateButton.visibility = View.VISIBLE
+                        }
+                        UpdateStatus.CHECKING_FOR_UPDATE -> {
+                            updateContainer.visibility = View.GONE
+                            infoContainer.visibility = View.GONE
+                            updateCheck.visibility = View.VISIBLE
+                            updateButton.visibility = View.GONE
+                        }
+                        UpdateStatus.FAILED_PREPARING_UPDATE,
+                        UpdateStatus.REPORTING_ERROR_EVENT,
+                        UpdateStatus.UPDATE_AVAILABLE,
+                        UpdateStatus.UPDATED_NEED_REBOOT -> {
+                            updateContainer.visibility = View.VISIBLE
+                            infoContainer.visibility = View.GONE
+                            updateCheck.visibility = View.GONE
+                            installSteps.visibility = View.GONE
+                            installProgress.visibility = View.GONE
+                            updateButton.visibility = View.VISIBLE
+                        }
+                        UpdateStatus.PREPARING_TO_UPDATE,
+                        UpdateStatus.DOWNLOADING,
+                        UpdateStatus.SUSPENDED,
+                        UpdateStatus.VERIFYING,
+                        UpdateStatus.FINALIZING -> {
+                            updateContainer.visibility = View.VISIBLE
+                            infoContainer.visibility = View.GONE
+                            updateCheck.visibility = View.GONE
+                            installProgress.apply {
+                                isIndeterminate = true
+                                visibility = View.VISIBLE
+                            }
+                            installSteps.visibility = View.VISIBLE
+                            updateButton.visibility = View.VISIBLE
+                        }
+                        else -> {
+                            Log.d(TAG, "Got an unexpected status: ${status.name}")
+                        }
+                    }
+                }
+            }
+
+            lifecycleScope.launchWhenStarted {
+                viewModel.updateProgress.collect {
+                    if (it != 0) {
+                        installProgress.apply {
+                            isIndeterminate = false
+                            progress = it
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/org/calyxos/systemupdater/ui/UpdateViewModel.kt
@@ -0,0 +1,191 @@
+/*
+ * Copyright (C) 2023 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.calyxos.systemupdater.ui
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.os.UpdateEngine
+import android.text.format.DateFormat
+import android.util.Log
+import androidx.core.content.edit
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.calyxos.systemupdater.R
+import org.calyxos.systemupdater.UpdateManager
+import org.calyxos.systemupdater.update.config.UpdateConfigRepository
+import org.calyxos.systemupdater.update.models.UpdateConfig
+import org.calyxos.systemupdater.update.models.UpdateStatus
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+import javax.inject.Inject
+
+@HiltViewModel
+@SuppressLint("StaticFieldLeak") // false positive, see https://github.com/google/dagger/issues/3253
+class UpdateViewModel @Inject constructor(
+    private val updateConfigRepository: UpdateConfigRepository,
+    private val gson: Gson,
+    private val sharedPreferences: SharedPreferences,
+    @ApplicationContext private val context: Context
+) : ViewModel() {
+
+    private val TAG = UpdateViewModel::class.java.simpleName
+    private val UPDATE_STATUS = "UpdateStatus"
+    private val UPDATE_CONFIG = "UpdateConfig"
+
+    private val updateManager = UpdateManager(UpdateEngine(), Handler(Looper.getMainLooper()))
+
+    private val _updateConfig = MutableStateFlow(UpdateConfig())
+    val updateConfig = _updateConfig.asStateFlow()
+
+    private val _updateStatus = MutableStateFlow(UpdateStatus.IDLE)
+    val updateStatus = _updateStatus.asStateFlow()
+
+    private val _updateProgress = MutableStateFlow(0)
+    val updateProgress = _updateProgress.asStateFlow()
+
+    private val _updateLastCheck = MutableStateFlow(getLastCheck())
+    val updateLastCheck = _updateLastCheck.asStateFlow()
+
+    init {
+        // restore last update status to properly reflect the status
+        restoreLastUpdate()
+
+        // set callbacks and bind the engine with app
+        updateManager.setOnStateChangeCallback { handleEngineStatus(it) }
+        updateManager.setOnProgressUpdateCallback { handleEngineProgress(it) }
+        updateManager.bind()
+    }
+
+    fun checkUpdates() {
+        _updateStatus.value = UpdateStatus.CHECKING_FOR_UPDATE
+        viewModelScope.launch {
+            val updateConfig = updateConfigRepository.getLatestUpdateConfig()
+            if (updateConfigRepository.newUpdateAvailable(updateConfig.buildDateUTC)) {
+                _updateConfig.value = updateConfig
+                _updateStatus.value = UpdateStatus.UPDATE_AVAILABLE
+            } else {
+                Log.d(TAG, "No new update available!")
+                _updateStatus.value = UpdateStatus.IDLE
+            }
+            _updateLastCheck.value = setLastCheck()
+        }
+    }
+
+    fun saveLastUpdate() {
+        _updateStatus.value.let {
+            when (it) {
+                UpdateStatus.IDLE, UpdateStatus.CHECKING_FOR_UPDATE -> {}
+                else -> {
+                    sharedPreferences.edit(true) {
+                        putString(UPDATE_STATUS, it.name)
+                        if (_updateConfig.value.name.isNotBlank()) {
+                            putString(UPDATE_CONFIG, _updateConfig.value.rawJson)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fun suspendUpdate() {
+        _updateStatus.value = UpdateStatus.SUSPENDED
+        updateManager.suspend()
+    }
+
+    fun resumeUpdate() {
+        restoreLastUpdate()
+        updateManager.resume()
+    }
+
+    fun applyUpdate() {
+        viewModelScope.launch {
+            if (_updateConfig.value.name.isNotBlank()) {
+                _updateStatus.value = UpdateStatus.DOWNLOADING
+                // TODO: Start the update
+            } else {
+                Log.d(TAG, "No new update to install!")
+            }
+        }
+    }
+
+    fun getASBDate(): String {
+        val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val securityVersionDate = simpleDateFormat.parse(Build.VERSION.SECURITY_PATCH)
+        val dateFormat = DateFormat.getLongDateFormat(context)
+        return dateFormat.format(securityVersionDate!!)
+    }
+
+    private fun restoreLastUpdate() {
+        val config = sharedPreferences.getString(UPDATE_CONFIG, String()) ?: String()
+        if (config.isNotBlank()) {
+            viewModelScope.launch(Dispatchers.IO) {
+                _updateConfig.value = gson.fromJson(config, UpdateConfig::class.java)
+                _updateConfig.value.rawJson = config
+            }
+        }
+
+        val status = sharedPreferences.getString(UPDATE_STATUS, UpdateStatus.IDLE.name)
+        status?.let { _updateStatus.value = UpdateStatus.valueOf(it) }
+    }
+
+    private fun handleEngineStatus(status: Int) {
+        when (val upStatus = UpdateStatus.values()[status + 1]) {
+            UpdateStatus.IDLE -> {
+                // update_engine can only get idle either due to error or successful update
+                when (_updateStatus.value) {
+                    UpdateStatus.DOWNLOADING,
+                    UpdateStatus.UPDATED_NEED_REBOOT -> { _updateStatus.value = upStatus }
+                    else -> {
+                        // do nothing
+                    }
+                }
+            }
+            UpdateStatus.CHECKING_FOR_UPDATE, UpdateStatus.UPDATE_AVAILABLE -> {
+                // do nothing for these status as they are controlled from app side
+            }
+            else -> { _updateStatus.value = upStatus }
+        }
+    }
+
+    private fun handleEngineProgress(progress: Double) {
+        _updateProgress.value = (100 * progress).toInt()
+    }
+
+    private fun getLastCheck(): String {
+        val notAvailable = context.getString(R.string.na)
+        return sharedPreferences.getString("last_check", notAvailable) ?: notAvailable
+    }
+
+    private fun setLastCheck(): String {
+        val simpleDateFormat = SimpleDateFormat("MMMM dd, yyyy kk:mm", Locale.getDefault())
+        val date: String = simpleDateFormat.format(Calendar.getInstance().time)
+        sharedPreferences.edit { putString("last_check", date) }
+        return date
+    }
+}
