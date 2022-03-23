9187c4fa3544bd52c0f4434184944c121f3a2e79
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-03-23 16:12:16 +0100

    Support fetching update config from remote server [2/3]
    This change introduces new UI for the Updater app that will fetch
    and showcase data from the remote server regarding updates.
    
    We are also switching to navigation components while at it with Kotlin
    to keep the app following Single Activity Arch as before.
    
    Change-Id: Ie990cdd820c5229518ccbeb948d0fbe019930ffe
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

---
+++ app/src/main/java/org/calyxos/systemupdater/update/models/UpdateStatus.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2023 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.calyxos.systemupdater.update.models
+
+/**
+ * Possible status of the Update
+ *
+ * This enum holds a combination of both status supplied by update_engine and
+ * custom ones required for this app. Status from update_engine are followed by custom.
+ */
+// Keep in sync with: system/update_engine/client_library/include/update_engine/update_status.h
+enum class UpdateStatus {
+    IDLE, // 0
+    CHECKING_FOR_UPDATE, // 1
+    UPDATE_AVAILABLE, // 2
+    DOWNLOADING, // 3
+    VERIFYING, // 4
+    FINALIZING, // 5
+    UPDATED_NEED_REBOOT, // 6
+    REPORTING_ERROR_EVENT, // 7
+    ATTEMPTING_ROLLBACK, // 8
+    DISABLED, // 9
+    NEED_PERMISSION_TO_UPDATE, // 10
+    CLEANUP_PREVIOUS_UPDATE, // 11
+    SUSPENDED, // custom: event when update is suspended
+    PREPARING_TO_UPDATE, // custom: event sent during payload verification, fetching props
+    FAILED_PREPARING_UPDATE // custom: event when payload verification or fetching props fails
+}
