a78ed2784214ce6809ea54769473c458931cae94
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-05-11 19:17:56 +0200

    Settings
    Change-Id: Ie7edbe50a070f4e7d60bfa2a89503e61a9090b3d

---
+++ src/org/calyxos/systemupdater/ui/SettingsActivity.java
@@ -0,0 +1,17 @@
+package org.calyxos.systemupdater.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings_container, new SettingsFragment())
+                .commit();
+    }
+}
