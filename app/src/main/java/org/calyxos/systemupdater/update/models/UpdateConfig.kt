d34f68505494a13cfe744ad6d48b1a42e2f1e353
Aayush Gupta <aayushgupta219@gmail.com>
2023-01-17 15:35:37 +0530

    Support fetching update config from remote server [1/3]
    This change introduces support for fetching update config from our
    remote server.
    
    This patch also adds the following libraries for convenience:
    - Dagger-Hilt for dependency injection
    - ktlint for formatting kotlin code
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I835bb5803d252f2a2ed045f0ab0c8c74a898f637

---
+++ app/src/main/java/org/calyxos/systemupdater/update/models/UpdateConfig.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2023 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.calyxos.systemupdater.update.models
+
+import com.google.gson.annotations.SerializedName
+
+data class UpdateConfig(
+    val name: String = String(),
+    val url: String = String(),
+    @SerializedName("changelog_url")
+    val changelogUrl: String = String(),
+    @SerializedName("build_date_utc")
+    val buildDateUTC: Long = 0L,
+    @SerializedName("ab_install_type")
+    val abInstallType: ABInstallType = ABInstallType.NOT_AVAILABLE,
+    @SerializedName("ab_config")
+    val abConfig: ABConfig = ABConfig(),
+    var rawJson: String = String()
+)
