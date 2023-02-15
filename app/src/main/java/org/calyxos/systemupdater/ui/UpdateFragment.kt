c456baa6504d736e45c950036603a85c92c666d2
Aayush Gupta <aayushgupta219@gmail.com>
2023-02-15 17:15:26 +0530

    Request REBOOT permission and set listner action on button
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I0d871e2dd39c3d110d83b3b79f12b98d117251e3

--- app/src/main/java/org/calyxos/systemupdater/ui/UpdateFragment.kt
+++ app/src/main/java/org/calyxos/systemupdater/ui/UpdateFragment.kt
@@ -18,6 +18,7 @@ package org.calyxos.systemupdater.ui
 
 import android.os.Build
 import android.os.Bundle
+import android.os.PowerManager
 import android.os.SystemProperties
 import android.util.Log
 import android.view.View
@@ -186,7 +187,8 @@ class UpdateFragment : Hilt_UpdateFragment(R.layout.fragment_update) {
                                 text = getString(R.string.reboot)
                                 visibility = View.VISIBLE
                                 setOnClickListener {
-                                    // TODO: Reboot device
+                                    val pm = context.getSystemService(PowerManager::class.java)
+                                    pm.reboot(null)
                                 }
                             }
                             networkWarning.visibility = View.GONE
