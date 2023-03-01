32dfa1350c1e0de85664594b8864b456cdbfd5e2
Aayush Gupta <aayushgupta219@gmail.com>
2023-03-01 16:15:35 +0530

    Support automated updates check
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I41635fd8f25024d1d3d338ce8d3e98a5609e6516

---
+++ app/src/main/java/org/calyxos/systemupdater/work/factory/UpdateWorkerFactory.kt
@@ -0,0 +1,25 @@
+package org.calyxos.systemupdater.work.factory
+
+import android.content.Context
+import androidx.work.ListenableWorker
+import androidx.work.WorkerFactory
+import androidx.work.WorkerParameters
+import javax.inject.Inject
+
+class UpdateWorkerFactory @Inject constructor(
+    private val updateAssistedFactory: UpdateAssistedFactory
+) : WorkerFactory() {
+
+    private val updateWorker = "UpdateWorker"
+
+    override fun createWorker(
+        appContext: Context,
+        workerClassName: String,
+        workerParameters: WorkerParameters
+    ): ListenableWorker? {
+        return when (workerClassName) {
+            updateWorker -> updateAssistedFactory.updateWorker(appContext, workerParameters)
+            else -> null
+        }
+    }
+}
