7949aff7dc065b6bf5b963da75cbdb116c188b68
Aayush Gupta <aayushgupta219@gmail.com>
2023-03-01 16:15:35 +0530

    Support automated updates check
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I41635fd8f25024d1d3d338ce8d3e98a5609e6516

---
+++ app/src/main/java/org/calyxos/systemupdater/work/UpdateWorker.kt
@@ -0,0 +1,32 @@
+package org.calyxos.systemupdater.work
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import org.calyxos.systemupdater.update.manager.UpdateManagerRepository
+import org.calyxos.systemupdater.util.CommonModule
+import java.util.Calendar
+import org.calyxos.systemupdater.service.SystemUpdaterService
+
+class UpdateWorker @AssistedInject constructor(
+    @Assisted val appContext: Context,
+    @Assisted workerParams: WorkerParameters,
+) : CoroutineWorker(appContext, workerParams) {
+
+    companion object {
+        const val WORK_NAME = "UpdateWork"
+    }
+
+    override suspend fun doWork(): Result {
+        Intent(appContext, SystemUpdaterService::class.java).also {
+            it.action = SystemUpdaterService.CHECK_AND_APPLY_UPDATES
+            appContext.startService(it)
+        }
+        return Result.success()
+    }
+}
