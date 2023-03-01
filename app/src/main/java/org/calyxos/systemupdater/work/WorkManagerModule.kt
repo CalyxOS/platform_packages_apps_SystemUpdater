fb66f0d101a91d8093aab94fed3920145d51340a
Aayush Gupta <aayushgupta219@gmail.com>
2023-03-01 16:15:35 +0530

    Support automated updates check
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I41635fd8f25024d1d3d338ce8d3e98a5609e6516

---
+++ app/src/main/java/org/calyxos/systemupdater/work/WorkManagerModule.kt
@@ -0,0 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.systemupdater.work
+
+import android.content.Context
+import androidx.work.WorkManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object WorkManagerModule {
+
+    /**
+     * Provides an instance of WorkManager
+     */
+    @Singleton
+    @Provides
+    fun provideWorkManagerInstance(@ApplicationContext context: Context): WorkManager {
+        return WorkManager.getInstance(context)
+    }
+}
