88f5e6eb181718809d6a7f1d7c08f1fa13ffead2
Aayush Gupta <aayushgupta219@gmail.com>
2023-03-10 13:02:04 +0530

    Merge UpdateConfig in UpdateManager package
    This makes UpdateManager single place from where anything
    concerning updates is handled.
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I15961f021efb8fb6a1beae22d7016686061e584c

--- app/src/main/java/org/calyxos/systemupdater/update/manager/UpdateManagerRepository.kt
+++ app/src/main/java/org/calyxos/systemupdater/update/manager/UpdateManagerRepository.kt
@@ -29,6 +29,10 @@ class UpdateManagerRepository @Inject constructor(
     val updateStatus = updateManagerImpl.updateStatus.asStateFlow()
     val updateProgress = updateManagerImpl.updateProgress.asStateFlow()
 
+    suspend fun getLatestUpdateConfig(): UpdateConfig? {
+        return updateManagerImpl.getUpdateConfig()
+    }
+
     fun suspendUpdate() {
         updateManagerImpl.suspendUpdate()
     }
