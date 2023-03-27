87b480acde4bf7b3392e853b564c929385922c9f
Aayush Gupta <aayushgupta219@gmail.com>
2023-03-27 23:18:24 +0530

    Support fetching update channels from system property
    Uses existing system property "sys.update.channel" to fetch
    update channels as well as updates it whenever user changes
    the update channel manually.
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I7475b929e46f64e5d7c4501ab17b78a1b229bc8b

---
+++ app/src/main/java/org/calyxos/systemupdater/util/CommonUtil.kt
@@ -0,0 +1,29 @@
+package org.calyxos.systemupdater.util
+
+import android.content.SharedPreferences
+import android.os.SystemProperties
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CommonUtil @Inject constructor(
+    private val sharedPreferences: SharedPreferences
+) {
+
+    companion object {
+        const val updateChannelProp = "sys.update.channel"
+    }
+
+    // OTA Update Channel Keys
+    private val channel = "channel"
+    private val stableChannel = "stable"
+    private val betaChannel = "beta"
+    private val channels = listOf(stableChannel, betaChannel)
+
+    fun getCurrentOTAChannel(): String {
+        val savedChannel = sharedPreferences.getString(channel, stableChannel) ?: stableChannel
+        val systemPropChannel = SystemProperties.get(updateChannelProp, savedChannel)
+        // Ensure that the channel coming from property is a valid channel
+        return if (systemPropChannel in channels) systemPropChannel else savedChannel
+    }
+}
