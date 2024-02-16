4dd53462dd980f56173d9cfb9a033e7e4142ce5c
Aayush Gupta <aayushgupta219@gmail.com>
2024-02-16 12:27:18 +0530

    gen_update_config: Generate a changelog_url property for changelogs
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I62fd05869f7668da5ad5292785f0260dea869744

--- tools/gen_update_config.py
+++ tools/gen_update_config.py
@@ -41,12 +41,14 @@ class GenUpdateConfig(object):
                  url,
                  ab_install_type,
                  ab_force_switch_slot,
-                 ab_verify_payload_metadata):
+                 ab_verify_payload_metadata,
+                 changelog_url):
         self.package = package
         self.url = url
         self.ab_install_type = ab_install_type
         self.ab_force_switch_slot = ab_force_switch_slot
         self.ab_verify_payload_metadata = ab_verify_payload_metadata
+        self.changelog_url = changelog_url
         self.streaming_required = (
             # payload.bin and payload_properties.txt must exist.
             'payload.bin',
@@ -73,6 +75,7 @@ class GenUpdateConfig(object):
             'url': self.url,
             'ab_config': self._gen_ab_config(),
             'ab_install_type': self.ab_install_type,
+            'changelog_url': self.changelog_url
         }
 
     def _gen_ab_config(self):
@@ -140,6 +143,9 @@ def main():  # pylint: disable=missing-docstring
     parser.add_argument('url',
                         type=str,
                         help='OTA package download url')
+    parser.add_argument('changelog_url',
+                        type=str,
+                        help='OTA package changelog url')
     args = parser.parse_args()
 
     if not args.out.endswith('.json'):
@@ -151,7 +157,8 @@ def main():  # pylint: disable=missing-docstring
         url=args.url,
         ab_install_type=args.ab_install_type,
         ab_force_switch_slot=args.ab_force_switch_slot,
-        ab_verify_payload_metadata=args.ab_verify_payload_metadata)
+        ab_verify_payload_metadata=args.ab_verify_payload_metadata,
+        changelog_url=args.changelog_url)
     gen.run()
     gen.write(args.out)
     print('Config is written to ' + args.out)
