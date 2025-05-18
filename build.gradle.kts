463fadf2e7e76c42426f6a29e9588027ebfe54e4
Aayush Gupta <aayushgupta219@gmail.com>
2025-05-18 17:40:47 +0800

    Bump AGP, Gradle, and dependencies to latest stable release
    Also bump compile, min, and target SDK levels while at it as this app is currently
    compiled in AOSP
    
    Change-Id: I9bf77dfebab2b7551df46f555becdc17a2c0ba40
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    // https://android.googlesource.com/platform/external/kotlinc/+/refs/tags/android-14.0.0_r61/build.txt
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.android.application") version "8.10.0" apply false
+    // https://android.googlesource.com/platform/external/kotlinc/+/refs/heads/android15-qpr2-release/build.txt
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0" apply false
 }
