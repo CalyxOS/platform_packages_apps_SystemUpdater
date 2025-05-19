4318a59ea84c2d394624658be7a9d0c1f32de84e
Aayush Gupta <aayushgupta219@gmail.com>
2025-05-19 15:09:26 +0800

    Migrate to gradle build version catalog
    https://developer.android.com/build/migrate-to-catalogs
    
    Change-Id: Ia950487f0a9458d122743697e0cd7c59b1d1312c
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,14 @@
 /*
- * SPDX-FileCopyrightText: 2022 The Calyx Institute
+ * SPDX-FileCopyrightText: 2022-2025 The Calyx Institute
  * SPDX-License-Identifier: Apache-2.0
  */
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp")
-    id("com.google.dagger.hilt.android")
-    id("org.jlleitschuh.gradle.ktlint")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.hilt.android.plugin)
+    alias(libs.plugins.jlleitschuh.ktlint)
 }
 
 android {
@@ -74,70 +74,26 @@ dependencies {
     compileOnly(fileTree(mapOf("dir" to "libs", "include" to listOf("android.jar", "libcore.jar"))))
 
     // AndroidX
-    // https://android.googlesource.com/platform/prebuilts/sdk/+/android15-qpr2-release/current/androidx/m2repository/androidx/core/core-ktx?autodive=0
-    implementation("androidx.core:core-ktx") {
-        version { strictly("1.16.0-alpha01") }
-    }
-    // https://android.googlesource.com/platform/prebuilts/sdk/+/android15-qpr2-release/current/androidx/m2repository/androidx/appcompat/appcompat?autodive=0
-    implementation("androidx.appcompat:appcompat") {
-        version { strictly("1.7.0") } // 1.8.0-alpha01 in AOSP but not released
-    }
-    // https://android.googlesource.com/platform/prebuilts/sdk/+/android15-qpr2-release/current/androidx/m2repository/androidx/fragment/fragment-ktx?autodive=0
-    implementation("androidx.fragment:fragment-ktx") {
-        version { strictly("1.8.6") } // 1.9.0-alpha01 in AOSP but not released
-    }
-    // https://android.googlesource.com/platform/prebuilts/sdk/+/android15-qpr2-release/current/androidx/m2repository/androidx/activity/activity-ktx?autodive=0
-    implementation("androidx.activity:activity-ktx") {
-        version { strictly("1.10.0-rc01") }
-    }
-    // https://android.googlesource.com/platform/prebuilts/sdk/+/android15-qpr2-release/current/androidx/m2repository/androidx/lifecycle/lifecycle-service?autodive=0
-    implementation("androidx.lifecycle:lifecycle-service") {
-        version { strictly("2.9.0-alpha08") }
-    }
-    // https://android.googlesource.com/platform/prebuilts/sdk/+/android15-qpr2-release/current/androidx/m2repository/androidx/lifecycle/lifecycle-viewmodel-ktx?autodive=0
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx") {
-        version { strictly("2.9.0-alpha08") }
-    }
-    // https://android.googlesource.com/platform/prebuilts/sdk/+/android15-qpr2-release/current/androidx/m2repository/androidx/preference/preference?autodive=0
-    implementation("androidx.preference:preference") {
-        version { strictly("1.2.1") } // 1.3.0-alpha01 in AOSP but as not been released
-    }
-    // https://android.googlesource.com/platform/prebuilts/sdk/+/android15-qpr2-release/current/androidx/m2repository/androidx/navigation/navigation-fragment-ktx?autodive=0
-    implementation("androidx.navigation:navigation-fragment-ktx") {
-        version { strictly("2.9.0-alpha04") }
-    }
-    // https://android.googlesource.com/platform/prebuilts/sdk/+/android15-qpr2-release/current/androidx/m2repository/androidx/navigation/navigation-ui-ktx?autodive=0
-    implementation("androidx.navigation:navigation-ui-ktx") {
-        version { strictly("2.9.0-alpha04") }
-    }
-    // https://android.googlesource.com/platform/prebuilts/sdk/+/android15-qpr2-release/current/androidx/m2repository/androidx/work/work-runtime-ktx?autodive=0
-    implementation("androidx.work:work-runtime-ktx") {
-        version { strictly("2.10.0-rc01") }
-    }
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.fragment)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.lifecycle.service)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.preference)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+    implementation(libs.androidx.work.runtime)
 
     // Google
-    // https://android.googlesource.com/platform/prebuilts/sdk/+/refs/heads/android15-qpr2-release/current/extras/material-design-x/Android.bp#7
-    implementation("com.google.android.material:material") {
-        version { strictly("1.13.0-alpha08") }
-    }
-    // https://android.googlesource.com/platform/external/guava/+/refs/heads/android15-qpr2-release/android/pom.xml
-    implementation("com.google.guava:guava") {
-        version { strictly("32.1.2-android") }
-    }
-    // https://android.googlesource.com/platform/external/gson/+/refs/heads/master/pom.xml
-    implementation("com.google.code.gson:gson") {
-        version { strictly("2.10.1") }
-    }
+    implementation(libs.google.android.material)
+    implementation(libs.google.guava)
+    implementation(libs.google.gson)
 
     // JetBrains
-    // https://android.googlesource.com/platform/external/kotlinx.coroutines/+/refs/heads/android15-qpr2-release/CHANGES.md
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android") {
-        version { strictly("1.8.1") }
-    }
+    implementation(libs.jetbrains.kotlin.coroutines)
 
-    // Hilt is an exception due to lack of any specific version source though newer versions requires newer kotlin releases
-    // https://android.googlesource.com/platform/external/dagger2/+/refs/heads/android15-qpr2-release
-    val hiltVersion = "2.51.1"
-    ksp("com.google.dagger:hilt-compiler:$hiltVersion")
-    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    // Hilt
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.hilt.android.core)
 }
