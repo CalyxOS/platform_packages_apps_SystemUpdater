1982652e0db5e4df0010d2968c676acec66692d8
Aayush Gupta <aayushgupta219@gmail.com>
2025-05-19 15:09:26 +0800

    Migrate to gradle build version catalog
    https://developer.android.com/build/migrate-to-catalogs
    
    Change-Id: Ia950487f0a9458d122743697e0cd7c59b1d1312c
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,13 @@
 /*
- * SPDX-FileCopyrightText: 2022 The Calyx Institute
+ * SPDX-FileCopyrightText: 2022-2025 The Calyx Institute
  * SPDX-License-Identifier: Apache-2.0
  */
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.10.0" apply false
-    // https://android.googlesource.com/platform/external/kotlinc/+/refs/heads/android15-qpr2-release/build.txt
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("com.google.devtools.ksp") version "1.9.23-1.0.20" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "11.0.0" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.google.ksp) apply false
+    alias(libs.plugins.hilt.android.plugin) apply false
+    alias(libs.plugins.jlleitschuh.ktlint) apply false
 }
