c3d78f98d2363dc483d89353b4de6d677ed28aa4
Aayush Gupta <aayushgupta219@gmail.com>
2025-05-19 15:27:54 +0800

    Migrate from gson to kotlinx.serialization
    kotlinx.serialization offers better null and type safety while avoiding
    reflection for serialization.
    
    Change-Id: If5af583bf3c83a5b2d6fc909ac3a81c9f7356717
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/org/calyxos/systemupdater/util/CommonModule.kt
+++ app/src/main/java/org/calyxos/systemupdater/util/CommonModule.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-FileCopyrightText: 2023-2025 The Calyx Institute
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -7,12 +7,12 @@ package org.calyxos.systemupdater.util
 
 import android.content.Context
 import android.content.SharedPreferences
-import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
 import javax.inject.Singleton
 
 @Module
@@ -32,11 +32,15 @@ object CommonModule {
     }
 
     /**
-     * Provides an instance of SharedPreferences
+     * Provides an instance of [Json]
      */
     @Singleton
     @Provides
-    fun provideGsonInstance(): Gson {
-        return Gson()
+    fun provideJsonInstance(): Json {
+        return Json {
+            prettyPrint = true
+            ignoreUnknownKeys = true
+            coerceInputValues = true
+        }
     }
 }
