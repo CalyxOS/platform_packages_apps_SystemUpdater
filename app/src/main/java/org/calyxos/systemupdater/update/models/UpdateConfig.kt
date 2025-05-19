0ae9310140ef669f87fe62f33b89f5565557d2a8
Aayush Gupta <aayushgupta219@gmail.com>
2025-05-19 15:27:54 +0800

    Migrate from gson to kotlinx.serialization
    kotlinx.serialization offers better null and type safety while avoiding
    reflection for serialization.
    
    Change-Id: If5af583bf3c83a5b2d6fc909ac3a81c9f7356717
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/org/calyxos/systemupdater/update/models/UpdateConfig.kt
+++ app/src/main/java/org/calyxos/systemupdater/update/models/UpdateConfig.kt
@@ -1,22 +1,24 @@
 /*
- * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-FileCopyrightText: 2023-2025 The Calyx Institute
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.calyxos.systemupdater.update.models
 
-import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class UpdateConfig(
     val name: String = String(),
     val url: String = String(),
-    @SerializedName("changelog_url")
+    @SerialName("changelog_url")
     val changelogUrl: String = String(),
-    @SerializedName("build_date_utc")
+    @SerialName("build_date_utc")
     val buildDateUTC: Long = 0L,
-    @SerializedName("ab_install_type")
+    @SerialName("ab_install_type")
     val abInstallType: ABInstallType = ABInstallType.NOT_AVAILABLE,
-    @SerializedName("ab_config")
+    @SerialName("ab_config")
     val abConfig: ABConfig = ABConfig(),
     var rawJson: String = String()
 )
