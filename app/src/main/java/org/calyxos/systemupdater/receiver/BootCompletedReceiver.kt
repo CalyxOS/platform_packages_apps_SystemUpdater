b84f0628f55a43f2d1bcbe7d8b062d797ff73a80
Aayush Gupta <aayushgupta219@gmail.com>
2025-05-29 00:07:58 +0800

    Rework preferences and related handling
    * Move all logic related to preference handling into single class
    * Ensure parity with legacy updater app preferences
    * Use Material3 theme for preferences where possible
    * Scrape out non-required hilt hacks for workmanager injection
    
    Issue: calyxos#3221
    Issue: calyxos#3268
    Issue: calyxos#3269
    Issue: calyxos#3270
    Change-Id: I8c1d0a7e30120f8100418a605efd2b4b3e410013
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

---
+++ app/src/main/java/org/calyxos/systemupdater/receiver/BootCompletedReceiver.kt
@@ -0,0 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2025 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.systemupdater.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import dagger.hilt.android.AndroidEntryPoint
+import org.calyxos.systemupdater.work.UpdateWorker
+
+/**
+ * Broadcast receiver to schedule automatic system updates on first boot of a device
+ */
+@AndroidEntryPoint(BroadcastReceiver::class)
+class BootCompletedReceiver : Hilt_BootCompletedReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        if (context != null && intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+            UpdateWorker.scheduleAutomatedUpdates(context)
+        }
+    }
+}
--- app/src/main/java/org/calyxos/systemupdater/ui/SettingsFragment.kt
+++ app/src/main/java/org/calyxos/systemupdater/ui/SettingsFragment.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-FileCopyrightText: 2023-2025 The Calyx Institute
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -13,17 +13,22 @@ import androidx.navigation.findNavController
 import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import com.google.android.material.appbar.MaterialToolbar
 import dagger.hilt.android.AndroidEntryPoint
 import org.calyxos.systemupdater.R
-import org.calyxos.systemupdater.util.CommonUtil
+import org.calyxos.systemupdater.util.PreferenceUtil
+import org.calyxos.systemupdater.util.PreferenceUtil.Companion.PREF_BATTERY
+import org.calyxos.systemupdater.util.PreferenceUtil.Companion.PREF_CHANNEL
+import org.calyxos.systemupdater.util.PreferenceUtil.Companion.PREF_NOTIFICATION
+import org.calyxos.systemupdater.work.UpdateWorker
 import javax.inject.Inject
 
 @AndroidEntryPoint(PreferenceFragmentCompat::class)
 class SettingsFragment : Hilt_SettingsFragment() {
 
     @Inject
-    lateinit var commonUtil: CommonUtil
+    lateinit var preferenceUtil: PreferenceUtil
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,21 +41,45 @@ class SettingsFragment : Hilt_SettingsFragment() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_preferences, rootKey)
 
-        findPreference<ListPreference>("channel")?.apply {
+        findPreference<ListPreference>(PREF_CHANNEL)?.apply {
+            when (val currentChannel = preferenceUtil.currentChannel) {
+                in resources.getStringArray(R.array.channel_values) -> {
+                    isEnabled = true
+                    summary = resources.getStringArray(R.array.channel_entries)[
+                        resources.getStringArray(R.array.channel_values).indexOf(currentChannel)
+                    ]
+                }
+
+                else -> {
+                    isEnabled = false
+                    summary = currentChannel
+                }
+            }
+
             setOnPreferenceChangeListener { _, newValue ->
-                summary = newValue.toString()
+                summary = resources.getStringArray(R.array.channel_entries)[
+                    resources.getStringArray(R.array.channel_values).indexOf(newValue)
+                ]
                 true
             }
-            summary = commonUtil.currentOTAChannel()
-            isEnabled = commonUtil.currentOTAChannel() in commonUtil.channels
         }
 
-        findPreference<Preference>("notifications")?.apply {
+        findPreference<Preference>(PREF_NOTIFICATION)?.apply {
             setOnPreferenceClickListener {
                 Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).also {
                     it.putExtra(Settings.EXTRA_APP_PACKAGE, context.packageName)
                     startActivity(it)
                 }
+                true
+            }
+        }
+
+        findPreference<SwitchPreferenceCompat>(PREF_BATTERY)?.apply {
+            setOnPreferenceChangeListener { _, newValue ->
+                UpdateWorker.updateAutomatedCheck(
+                    context = requireContext(),
+                    requiresBatteryNotLow = newValue.toString().toBoolean()
+                )
                 true
             }
         }
