5cc12d39d893ebb4de018cd4f29862a470b93007
Tommy Webb <tommy@calyxinstitute.org>
2025-05-29 13:36:38 -0400

    Support Direct Boot
    Handle updates before first unlock, too.
    
    Issue: calyxos#3272
    Change-Id: I6e03e059a383259551c4cdc86add016d79ebf77f

--- app/src/main/java/org/calyxos/systemupdater/SystemUpdaterApp.kt
+++ app/src/main/java/org/calyxos/systemupdater/SystemUpdaterApp.kt
@@ -9,4 +9,8 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp(Application::class)
-class SystemUpdaterApp : Hilt_SystemUpdaterApp()
+class SystemUpdaterApp : Hilt_SystemUpdaterApp() {
+    // WorkManager crashes when it knows you are trying to use it in Direct Boot; however,
+    // it should work. Below is a workaround. See https://issuetracker.google.com/issues/112773820
+    override fun isDeviceProtectedStorage() = false
+}
