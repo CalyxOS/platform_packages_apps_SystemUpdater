23ac4ad443034bd296665c6a9bea23cf27aecc3f
Chirayu Desai <chirayudesai1@gmail.com>
2025-06-02 20:07:58 +0530

    models: Final metadata
    * Remove unused metadata
    * Add all the final metadata
    
    Issue: calyxos#782
    Change-Id: I5d3585bb1f7e6696861fcf984e45a9fdecfdc2f6

--- app/src/main/java/org/calyxos/systemupdater/update/models/UpdateConfig.kt
+++ app/src/main/java/org/calyxos/systemupdater/update/models/UpdateConfig.kt
@@ -12,14 +12,22 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class UpdateConfig(
     val name: String = String(),
-    val url: String = String(),
-    @SerialName("changelog_url")
-    val changelogUrl: String = String(),
+    @SerialName("calyxos_version")
+    val calyxOSVersion: String = String(),
+    @SerialName("android_version")
+    val androidVersion: String = String(),
     @SerialName("build_date_utc")
     val buildDateUTC: Long = 0L,
-    @SerialName("ab_install_type")
-    val abInstallType: ABInstallType = ABInstallType.NOT_AVAILABLE,
-    @SerialName("ab_config")
-    val abConfig: ABConfig = ABConfig(),
-    var rawJson: String = String()
+    @SerialName("build_id")
+    val buildId: String = String(),
+    @SerialName("build_number")
+    val buildNumber: String = String(),
+    @SerialName("changelog_url")
+    val changelogUrl: String = String(),
+    @SerialName("security_patch_level")
+    val securityPatchLevel: String = String(),
+    @SerialName("required_builds")
+    val requiredBuilds: List<String> = emptyList(),
+    @SerialName("zips")
+    val zips: List<ABConfig> = emptyList()
 )
