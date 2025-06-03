770c804d0e63b0f0cfa8f098d819b836094b862b
Tommy Webb <tommy@calyxinstitute.org>
2025-06-03 09:53:18 -0400

    gen_update_config: Remove unused arguments
    Issue: calyxos#782
    Change-Id: If58a01ff7a059f233f72929bad44dcb4ae89162a

--- tools/gen_update_config.py
+++ tools/gen_update_config.py
@@ -33,33 +33,13 @@ class GenUpdateConfig(object):
     TODO: add non-A/B packages support.
     """
 
-    AB_INSTALL_TYPE_STREAMING = 'STREAMING'
-    AB_INSTALL_TYPE_NON_STREAMING = 'NON_STREAMING'
-
     def __init__(self,
                  package,
                  url,
-                 ab_install_type,
-                 ab_force_switch_slot,
-                 ab_verify_payload_metadata,
                  changelog_url):
         self.package = package
         self.url = url
-        self.ab_install_type = ab_install_type
-        self.ab_force_switch_slot = ab_force_switch_slot
-        self.ab_verify_payload_metadata = ab_verify_payload_metadata
         self.changelog_url = changelog_url
-        self.streaming_required = (
-            # payload.bin and payload_properties.txt must exist.
-            'payload.bin',
-            'payload_properties.txt',
-        )
-        self.streaming_optional = (
-            # care_map.txt is available only if dm-verity is enabled.
-            'care_map.txt',
-            # compatibility.zip is available only if target supports Treble.
-            'compatibility.zip',
-        )
         self._config = None
 
     @property
@@ -71,10 +51,9 @@ class GenUpdateConfig(object):
         """Generates config."""
         self._config = {
             '__': '*** Generated using tools/gen_update_config.py ***',
-            'name': self.ab_install_type[0] + ' ' + os.path.basename(self.package)[:-4],
+            'name': os.path.basename(self.package)[:-4],
             'url': self.url,
             'ab_config': self._gen_ab_config(),
-            'ab_install_type': self.ab_install_type,
             'changelog_url': self.changelog_url
         }
 
@@ -83,8 +62,6 @@ class GenUpdateConfig(object):
         with zipfile.ZipFile(self.package, 'r') as package_zip:
             config = {
                 'property_files': self._get_property_files(package_zip),
-                'verify_payload_metadata': self.ab_verify_payload_metadata,
-                'force_switch_slot': self.ab_force_switch_slot,
             }
 
         return config
@@ -114,26 +91,8 @@ class GenUpdateConfig(object):
 
 
 def main():  # pylint: disable=missing-docstring
-    ab_install_type_choices = [
-        GenUpdateConfig.AB_INSTALL_TYPE_STREAMING,
-        GenUpdateConfig.AB_INSTALL_TYPE_NON_STREAMING]
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('--ab_install_type',
-                        type=str,
-                        default=GenUpdateConfig.AB_INSTALL_TYPE_NON_STREAMING,
-                        choices=ab_install_type_choices,
-                        help='A/B update installation type')
-    parser.add_argument('--ab_force_switch_slot',
-                        default=False,
-                        action='store_true',
-                        help='if set device will boot to a new slot, otherwise user '
-                              'manually switches slot on the screen')
-    parser.add_argument('--ab_verify_payload_metadata',
-                        default=False,
-                        action='store_true',
-                        help='if set the app will verify the update payload metadata using '
-                             'update_engine before downloading the whole package.')
     parser.add_argument('package',
                         type=str,
                         help='OTA package zip file')
@@ -155,9 +114,6 @@ def main():  # pylint: disable=missing-docstring
     gen = GenUpdateConfig(
         package=args.package,
         url=args.url,
-        ab_install_type=args.ab_install_type,
-        ab_force_switch_slot=args.ab_force_switch_slot,
-        ab_verify_payload_metadata=args.ab_verify_payload_metadata,
         changelog_url=args.changelog_url)
     gen.run()
     gen.write(args.out)
