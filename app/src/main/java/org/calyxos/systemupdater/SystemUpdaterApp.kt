e05bc317e4b75e1c9cc3921faffb083d9cacc3d7
Aayush Gupta <aayushgupta219@gmail.com>
2025-06-04 14:32:53 +0800

    Strip out notification related logic to separate class
    * Create notification channel on application startup
    
    Change-Id: I58b54ef6474ca200106ec99979b75c2ef1e899e5
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/org/calyxos/systemupdater/SystemUpdaterApp.kt
+++ app/src/main/java/org/calyxos/systemupdater/SystemUpdaterApp.kt
@@ -6,11 +6,25 @@
 package org.calyxos.systemupdater
 
 import android.app.Application
+import android.app.NotificationManager
+import androidx.core.content.getSystemService
 import dagger.hilt.android.HiltAndroidApp
+import org.calyxos.systemupdater.util.NotificationUtil
 
 @HiltAndroidApp(Application::class)
 class SystemUpdaterApp : Hilt_SystemUpdaterApp() {
+
+    private val notificationManager: NotificationManager
+        get() = this.getSystemService<NotificationManager>()!!
+
     // WorkManager crashes when it knows you are trying to use it in Direct Boot; however,
     // it should work. Below is a workaround. See https://issuetracker.google.com/issues/112773820
     override fun isDeviceProtectedStorage() = false
+
+    override fun onCreate() {
+        super.onCreate()
+        notificationManager.createNotificationChannels(
+            NotificationUtil.getNotificationChannels(this)
+        )
+    }
 }
